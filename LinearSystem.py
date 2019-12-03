5e076cdf894b392aa615252ffca8d1351c1558f6
Ramin Abbasi <Rabbasi.mech@gmail.com>
2019-12-03 15:55:05 +0330

    Add runsys function in ls to solve a system
    added runsys with three methods of 'numerical', 'exact', and 'discrete'
    solutions

--- LinearSystem.py
+++ LinearSystem.py
@@ -3,6 +3,7 @@ import sympy as sym
 import numpy as np
 from numpy import math as mth
 import scipy.linalg as la
+import scipy.integrate as integrate
 
 def MatFun(mat,f,x):
     idx = [0]
@@ -24,5 +25,44 @@ def MatFun(mat,f,x):
     F_Mat = sym.Matrix.diag(*Block)
     F_mat = U.dot(F_Mat).dot(la.inv(U))
     
-    return F_mat
-# 
+    return F_mat, F_Mat
+
+def runsys(A,**kwargs):
+    B = kwargs.get('B',np.zeros(A.shape))
+    u = kwargs.get('u',np.zeros((A.shape[0],1)))
+    show = kwargs.get('show',[''])
+    method = kwargs.get('method','numerical')
+    step = kwargs.get('step',0.1)
+    time = kwargs['t']
+    x0 = kwargs['x0']
+    
+    if method == 'numerical':
+        sys = lambda x,t : np.squeeze(A.dot(np.array([x]).transpose())+B.dot(u))
+        sol = integrate.odeint(sys,x0,time)
+    elif method == 'exact':
+        l = sym.Symbol('l')
+        t = sym.Symbol('t')
+        f = sym.exp(l*t)
+        x0 = sym.Matrix(x0)
+        f_A = sym.Matrix(MatFun(A,f,l)[0]) * x0
+        solFun = lambda x : np.array(f_A.subs(t,x).evalf())
+        sol = np.concatenate(list(map(solFun, time)),axis = 1).transpose()
+    elif method == 'discrete':
+        l = sym.Symbol('l')
+        f = sym.exp(l*step)        
+        Ad = MatFun(A,f,l)[0]
+        sol = np.zeros((2,time.shape[0]))
+        sol[:,0] = np.array([x0])
+        for i in range(1,time.shape[0]):
+            sol[:,i] = np.squeeze(Ad.dot(sol[:,i-1][:,np.newaxis]))
+            
+        sol = sol.transpose()
+    if 'Jordan' in show:
+        print(vs.Transform(A,'J')[1])
+    elif 'Solution' in show :
+        L = sym.Symbol('l')
+        T = sym.Symbol('t')
+        f = sym.exp(L*T)
+        print(MatFun(A,f,L)[1])
+
+    return sol
