67509f67d71f769ce7f945cbc99dd98b77e61e34
Ramin Abbasi <Rabbasi.mech@gmail.com>
2021-03-14 13:06:46 +0330

    removed matrix functions completely, rewriting

--- VectorSpace.py
+++ VectorSpace.py
@@ -11,7 +11,7 @@ matrices. It requires sympy, numpy, scipy, and re packages.
 Classes
 =======
 VS includes two main classes:
-    1) linMap (and operator)
+    1) linMap
     2) vector
 
 linMap
@@ -55,94 +55,13 @@ getD(space) - returns the dimension of 'space'
 U(vBase,wBase) - returns the Unitary transformation from 'vBase' to 'wBase'
 isindep(bList) - checks the linear independence of 'bList'
 sym2num(Mat) - turns the sympy matrix into numpy array
-findBasis(T,mat) - turns the basis on which T has the form mat
-
-Functions on Matrices
-=====================
-
-
-Public methods
---------------
-eig(mat,mode) - calculates eigenvalues and generalized eigenvectors  
-
-    Inputs: (mat,mode)
-    -------
-    mat  - input matrix
-    mode - specifies the structure (D = Diagonal, J = Jordan)
-
-    output: (gen_eigvec,gen_eigval)
-    -------
-    gen_eigval : returns 'a : (b,c,d)', where
-        a : eigenvalue of 'mat'
-        b : algebraic multiplicity of a
-        c : geometric multiplicity of a
-        d : the power of (z-a) in minimum polynomial
-    gen_eigvec : returns '{a : array}'
-        a : eigenvalue of 'mat'
-        array : generalized eigenvectors of a
-    
-
-Transform(mat,form,field) - Transforms the matrix into a desired form
-    
-    Input: (mat,form,field)
-    ------
-    mat  : input matrix
-    form : specifies the required form (D = Diagonal, J = Jordan)
-    field: specifies the working field (C = Complex, R = Real)
-
-    Output: (U,Mat)
-    -------
-    U : transformation matrix
-    Mat : transformed matrix
-    
-invTransform(mat,U) - Transforms the matrix back using U
-    
-    Input: (mat,U)
-    ------
-    mat : transformed matrix
-    U: transformation matrix
-    
-    Output: Mat
-    -------
-    Mat : transformed matrix
-    
-SVD(mat, vBase, wBase) - Calculate the Singular-Value-Decomposition 'W*Sigma*V^-1'
-
-    Input: mat
-    ------
-    mat : input matrix
-    
-    Output: (W,Sigma,V)
-    -------
-    W : left singular vectors
-    Sigma : diagonal matrix of singular values
-    V : right singular vectors
-    
-Polar(mat) - Calcualte the Polar decomposition using svd 'U*P'
-
-    Input: mat
-    ------
-    mat : input matrix
-    
-    Output: (U,P)
-    -------
-    U : Isometry
-    P : Positive Operator
-
-Private methods
----------------
-_setR(gen_eigvec,gen_eigval) - restricts the basis computation to Field R
-_GenEigen(Base,nil,num,mode) - returns the correct basis for D or J form
-_makebasis(arr,n) - rearranges the basis to have a uniform structure
-_realize(mat) - turns a complex matrix 'mat' into real if possible
-_expand(mat) - expand each entry of the matrix by multiplying it by I
 """
 
 
 
 import sympy as sym
 import numpy as np
-import scipy.linalg as la
+#import scipy.linalg as la
 import re
 sym.init_printing(pretty_print=False)
 
@@ -180,20 +99,13 @@ class linMap:
     def null(self):
         return [invMatv(M,basis(self.V)) for M in Mat(self,basis(self.V),basis(self.W)).nullspace()]
     def eig(self):
-        M = la.eig(sym2num(Mat(self,basis(self.V),basis(self.W))))
-        mat = {}
-        for i in range(len(M[0])) : mat[M[0][i]] = []
-        for i in range(len(M[0])):
-            mat[M[0][i]].append(invMatv(M[1][:,i],basis(self.V)))
-        return mat
+        M = Mat(self,basis(self.V),basis(self.W)).eigenvals()
+        print(M)
+        return 0
     def Adj(self):
         vBase = basis(self.V)
         wBase = basis(self.W)
         return invMat(sym.Matrix.adjoint(Mat(self,vBase,wBase)),wBase,vBase)
-
-class operator(linMap):
-    def __init__(self,fun,V):
-        super().__init__(fun,V,V)
 
 class vector:
     def __init__(self,array,space):
@@ -219,7 +131,7 @@ class vector:
         if re.match(r'F.',self.space):
             return sum(self.vec * other.vec)
         if re.match(r'P.',self.space):
-            return sym.integrate(self.vec*other.vec,(sym.Symbol('x'),-np.pi,np.pi))
+            return sym.integrate(self.vec*other.vec,(sym.Symbol('x'),-sym.pi,sym.pi))
     def norm(self):
         return sym.sqrt(self.innerproduct(self))
     def normalize(self):
@@ -249,14 +161,18 @@ def eye(space):
     I = invMat(sym.eye(n),basis(space),basis(space))
     return I
 
-def basis(space):
+def basis(space, gramm = 1):
     n = getD(space)
     if re.match(r'F.',space):
         base = sym.Matrix.diag([1]*n)
-        return Gramm(vectors([base.row(i)[:] for i in range(n)],space))
+        B = vectors([base.row(i)[:] for i in range(n)],space)
+        if gramm == 1: return Gramm(B)
+        if gramm == 0: return B
     if re.match(r'P.',space):
         x = sym.Symbol('x')
-        return Gramm(vectors([x**i for i in range(n)],space))
+        B = vectors([x**i for i in range(n)],space)
+        if gramm == 1: return Gramm(B)
+        if gramm == 0: return B
 
 def Gramm(base):
     eBase = base.copy()
@@ -266,16 +182,19 @@ def Gramm(base):
                     for j in range(i)],base[0].initial())).normalize()
     return eBase
 
-def Matv(vec,base):
-    Mvec = np.zeros((1,1))
+def Matv(v,base):
     if re.match(r'P.',base[0].space):
-        for b in base[1:]:
-            Mvec = np.append(Mvec,vec.vec.coeff(b.vec))
-        Mvec[0] = (vec - Mvec.dot(base)).vec
-        return sym.Matrix(Mvec)
+        x = sym.Symbol('x')
+        base_sum = sum([b.vec for b in base])
+        scale = np.array(sym.Poly(base_sum,x).all_coeffs())
+        coef = np.array(sym.Poly(v.vec,x).all_coeffs())
+        coef_pad = np.zeros(scale.shape)
+        coef_pad[scale.shape[0]-coef.shape[0]:] = coef
+        mat = coef_pad/scale
+        return sym2num(mat[scale !=0][::-1])[:,np.newaxis]
     if re.match(r'F.',base[0].space):
-        B = sym.Matrix([b.vec for b in base]).transpose()
-        return sym.Matrix(B.inv()*(vec.vec[:,np.newaxis]))
+        B = sym.Matrix([(b.vec).T for b in base]).T
+        return sym2num(B.inv()*(v.vec)[:,np.newaxis])
     
 def invMatv(mat,base):
     if mat != []:
@@ -286,7 +205,7 @@ def invMatv(mat,base):
 def Mat(lMap,vBase=0,wBase=0):
     if vBase == 0 : vBase = basis(lMap.V)
     if wBase == 0 : wBase = basis(lMap.W)
-    return sym.Matrix.hstack(*[Matv(lMap(VBase),wBase) for VBase in vBase])
+    return np.concatenate([Matv(lMap(VBase),wBase) for VBase in vBase],axis=1)
 
 def invMat(mat,vBase=0,wBase=0):
     if vBase == 0 : vBase = basis("F{}".format(mat.shape[1]))
@@ -316,156 +235,5 @@ def isindep(bList):
         return 1
 
 def sym2num(Mat):
-    return _realize(np.array(Mat).astype(np.float64))
+    return np.array(Mat).astype(np.float64)
 
-def findBase(T,mat):
-    M = sym2num(Mat(T))
-    L = la.block_diag(*[M]*M.shape[0]) - _expand(mat.transpose())
-    V = np.sum(la.null_space(L),axis=1)
-    if V!=[]:
-        return vectors(np.hsplit(V,getD(T.V)),T.V)
-    else:
-        print("couldn't find a basis")
-        return []
-
-#========================================================================= 
-# Matrix Functions
-#========================================================================= 
-eps = 1e-6
-
-def eig(mat,mode = 'D'):
-    Eigen = la.eig(mat)
-#    eigval = np.sort(np.unique(Eigen[0]))[::-1]
-    eigval = np.sort(np.unique(np.around(Eigen[0],decimals = 15)))[::-1]
-    eigval = _realize(eigval) #Test
-    gen_eigvec = {}
-    gen_eigval = {}
-    for ev in eigval:
-        eigvec = []
-        nil = mat - ev*np.eye(mat.shape[0])
-        alg_mlt = len(np.where(np.abs(Eigen[0]-ev)<eps)[0])
-        geo_mlt = la.null_space(nil,rcond = eps).shape[1]
-        for i in range(alg_mlt):
-            G = la.null_space(_realize(np.linalg.matrix_power(nil,i+1)),rcond = eps)
-            eigvec.append(G)
-            if G.shape[1] == alg_mlt:
-                gen_eigval[ev] = (alg_mlt,geo_mlt,i+1)
-                break
-        base = _makebasis(_realize(np.concatenate(eigvec,axis=1)),alg_mlt)
-        gen_eigvec[ev] = _GenEigen(base,nil,gen_eigval[ev],mode)
-    return gen_eigvec, gen_eigval
-
-def _setR(gen_eigvec,gen_eigval):
-    num = 1
-    for ev in gen_eigvec.keys():
-        if not(abs(ev.imag-0)<eps):
-            if num == 1:
-                gen_eigvec[ev] = 2*gen_eigvec[ev].real
-                num = num + 1
-            else:
-                gen_eigvec[ev] = 2*gen_eigvec[ev].imag
-                num = num + 1
-        if num == 3: num = 1
-    return gen_eigvec, gen_eigval
-
-def _GenEigen(Base,nil,num,mode):
-
-    alg_mlt, geo_mlt, nil_pwr = num
-    vList = np.hsplit(Base,Base.shape[1])
-    idx = []
-    G = {}
-    J = []
-    for i in range(len(vList)):
-        for n in range(1,nil_pwr+1):
-            w = np.linalg.matrix_power(nil,n).dot(vList[i])
-            if (_realize(w) == 0).all():
-                idx.append((i,n))
-                break
-    idx_eig = [i for i, x in enumerate(idx) if x[1] == 1]
-    
-    for indice in idx_eig:
-        G[indice] = [vList[indice]]
-        for i,v in enumerate(vList):
-            w = np.linalg.matrix_power(nil,idx[i][1]-1).dot(v)
-            cmat = np.concatenate([vList[indice],w],axis = 1)
-            det = np.abs(np.linalg.det(cmat.transpose().dot(cmat)))
-            if (det < eps) and idx[i][1]!=1 :
-                G[indice].append(vList[i])
-        G[indice] = np.concatenate(G[indice],axis=1)
-        
-    for i in G.keys():
-        for j in range(G[i].shape[1]):
-            Null = (np.linalg.matrix_power(nil,j).dot(G[i]))
-            J.append(Null[:,-1][:,np.newaxis])
-    J_Base = _realize(np.concatenate(J[::-1],axis=1))
-    D_Base = np.concatenate(list(G.values()),axis = 1)
-    if mode == 'J':
-        return J_Base
-    elif mode == 'D':
-        return D_Base
-    else:
-        return D_Base
-       
-def _makebasis(arr,n): 
-    base = arr[:,min(np.where((arr != 0))[1])][:,np.newaxis]
-    i = 1
-    while base.shape[1] < n:
-        cmat = np.concatenate([base,arr[:,i][:,np.newaxis]],axis=1)
-        if np.abs(np.linalg.det(cmat.transpose().dot(cmat))) > eps :
-            base = cmat
-        i = i+1
-    return base
-    
-def _realize(mat):
-    mat.real[np.absolute(np.real(mat))< eps]=0
-    if np.array_equal(np.real(mat),mat):
-        return np.real(mat)
-    else:
-        mat.imag[np.absolute(np.imag(mat))< eps]=0
-    return mat
-
-def _expand(mat):
-    n = mat.shape[0]
-    return np.vstack([np.block([np.eye(n)]*n)*np.repeat(mat[i,:],n) for i in range(n)])
-
-def Transform(mat,form = 'D',field = 'C'):
-    if field == 'R':
-        Evec, Eval = _setR(*eig(mat,form))
-    elif field == 'C':
-        Evec, Eval = eig(mat,form)
-    else:
-        print('Field can only be C or R')
-        return 'Error';
-    U = np.concatenate(list(Evec.values()),axis=1)
-    Mat = la.inv(U).dot(mat).dot(U)
-    return U,_realize(Mat)   
-
-def invTransform(mat,U):
-    return U.dot(mat).dot(la.inv(U))
-
-def SVD(mat, vBase = 0, wBase = 0):
-    if vBase == 0 : vBase = basis("F{}".format(mat.shape[1]))
-    if wBase == 0 : wBase = basis("F{}".format(mat.shape[0]))
-    U_vBase = sym2num(U(vBase,basis("F{}".format(mat.shape[1]))))
-    U_wBase = sym2num(U(wBase,basis("F{}".format(mat.shape[0])))) 
-    M = U_wBase.dot(mat).dot(np.linalg.inv(U_vBase))
-    MtM = M.transpose().conjugate().dot(M)
-    MMt = M.dot(M.transpose().conjugate())
-    E = eig(MtM)[0]
-    F = eig(MMt)[0]
-    e = []
-    f = []
-    for sv in list(E.keys()):
-        e.append(E[sv])
-        f.append(F[sv])
-    Ue = np.concatenate(e,axis=1)
-    Uf = np.concatenate(f,axis=1)
-    return np.linalg.inv(U_wBase).dot(Uf),\
-           Uf.transpose().dot(M).dot(Ue),\
-           np.linalg.inv(U_vBase).dot(Ue)
-
-def Polar(mat, vBase = 0, wBase = 0):
-    W,Sigma,V = SVD(mat,vBase,wBase)
-    P = V.dot(Sigma).dot(np.linalg.inv(V))
-    U = W.dot(np.linalg.inv(V))
-    return U,P
